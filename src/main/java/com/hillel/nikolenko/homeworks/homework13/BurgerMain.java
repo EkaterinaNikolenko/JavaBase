2b7e394a73f60496607e36cd4d4accc52e0b86fe
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-03-08 19:12:42 +0200

    hw12

---
+++ src/main/java/com/hillel/nikolenko/homeworks/homework13/BurgerMain.java
@@ -0,0 +1,10 @@
+package com.hillel.nikolenko.homeworks.homework13;
+
+public class BurgerMain {
+    public static void main(String[] args) {
+        Burger burger1 = new Burger("bun with sesame", "beef", "cheddar cheese", "cabbage", "mayonnaise");
+        Burger burger2 = new Burger("bun with sesame", "beef", "cheddar cheese", "cabbage");
+        Burger burger3 = new Burger("bun with sesame", "beef", 2, "cheddar cheese", "cabbage");
+        Burger burger4 = new Burger("bun with sesame", "beef", 2, "cheddar cheese", "cabbage", "mayonnaise");
+    }
+}
