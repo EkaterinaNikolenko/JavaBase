5c5f2a68e95fe87494313a7d2618a5be232d07b6
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-03-11 16:33:26 +0200

    hw14

---
+++ src/main/java/com/hillel/nikolenko/homeworks/homework14/Main.java
@@ -0,0 +1,23 @@
+package com.hillel.nikolenko.homeworks.homework14;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Steve", 1, 3, 2000, "user1@gmail.com", "0123456789", "Johnson", 73.5,120, 10000);
+        User user2 = new User("John", 13, 5, 1994, "user2@gmail.com", "9876543210", "Smith", 81.2,125, 12500);
+        User user3 = new User("Robert", 26, 7, 2011, "user3@gmail.com", "1357924680", "Williams", 35.0,117, 15000);
+
+        user1.printAccountInfo();
+        System.out.println();
+        user2.printAccountInfo();
+        System.out.println();
+        user3.printAccountInfo();
+        System.out.println();
+
+        user1.setSteps(11350);
+        user3.setWeight(38.0);
+
+        user1.printAccountInfo();
+        System.out.println();
+        user3.printAccountInfo();
+    }
+}
---
+++ src/main/java/com/hillel/nikolenko/homeworks/homework14/User.java
@@ -0,0 +1,100 @@
+package com.hillel.nikolenko.homeworks.homework14;
+
+public class User {
+    private String firstname;
+    private int day;
+    private int month;
+    private int year;
+    private String email;
+    private String telephoneNumber;
+    private String lastname;
+    private double weight;
+    private int pressure;
+    private int steps;
+    private int age;
+    public User(String firstname, int day, int month, int year, String email, String telephoneNumber, String lastname, double weight, int pressure, int steps) {
+        this.firstname = firstname;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        age = 2023 - year;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.lastname = lastname;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.steps = steps;
+    }
+
+    void printAccountInfo(){
+        System.out.println("first name: " + firstname);
+        System.out.println("last name: " + lastname);
+        System.out.println("date of birth: " + day + "." + month + "." + year);
+        System.out.println("age: " + age);
+        System.out.println("email: " + email);
+        System.out.println("telephone: " + telephoneNumber);
+        System.out.println("weight: " + weight);
+        System.out.println("pressure: " + pressure);
+        System.out.println("steps: " + steps);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+}
